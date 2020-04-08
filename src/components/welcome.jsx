2aa3579fa975c0f1b2be86eff1f3c0cda039cbb5
github username <laurenkyu@gmail.com>
2020-04-08 17:55:53 -0400

    end of day 1 backend set

---
+++ src/components/welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../Welcome.css';
+
+
+class Welcome extends React.Component {
+
+    render() {
+        return (
+            <div className="welcome-page">
+                <h1 id="welcome-banner">Welcome!</h1>
+            </div>
+        )
+    }
+}
+
+export default Welcome
