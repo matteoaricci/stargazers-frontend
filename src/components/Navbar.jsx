7f7c4591d6c0501b05c3dae827475b1b610a34c1
briannalturner <blturner3527@gmail.com>
2020-04-09 11:19:35 -0400

    added star logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,7 @@ class Navbar extends React.Component {
         return (
             <div>
                 <nav className="navbar navbar-light bg-light">
-                    <a className="navbar-brand" href="#">
-                        <img src="/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt="" />
-                        Stargazers
-                    </a>
+                    <a className="navbar-brand" href="#"><img src="https://www.pngarts.com/files/3/Mario-Star-PNG-Image.png" width="30" height="30" className="d-inline-block align-top" alt="" /> Stargazers</a>
                 </nav>
             </div>
         )
