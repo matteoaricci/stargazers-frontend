1ea3366b9bf064bc07418e60f63367b65c14e1f0
github username <laurenkyu@gmail.com>
2020-04-09 23:39:46 -0400

    added first constellation images to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,17 +4,35 @@ import '../home.css';
 
 class Home extends React.Component {
 
+    constructor() {
+        super()
+        this.state = {
+            currentImage: "big-dipper-pixel"
+        }
+    }
+
     render() {
         return (
             <div>
                 <div className="welcome-page">
                     <h1 id="welcome-banner">Welcome!</h1>
+                    <div onMouseEnter = {this.handleMouseEnter} onMouseLeave = {this.handleMouseLeave}> 
+                        <img src={require(`../assets/pictures/constellations/${this.state.currentImage}.png`)} alt = "big dipper"/>
+                    </div>
                 </div>
                 <div>
                     {/* <img src={require("../assets/pictures/uranus-pixel.png")} alt=""/> */}
                 </div>
             </div>
         )
+    }
+
+    handleMouseEnter = () => {
+        this.setState({currentImage: "big-dipper-pixel-connected"})
+    }
+
+    handleMouseLeave = () => {
+        this.setState({currentImage: "big-dipper-pixel"})
     }
 }
 
